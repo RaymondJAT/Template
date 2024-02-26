283e1bacc1143385e6fa021d1aeb873620600997
RaymondJAT <rjat081700@gmail.com>
2024-02-27 00:10:17 +0800

    update

--- first-project/src/components/about/AboutSection.jsx
+++ first-project/src/components/about/AboutSection.jsx
@@ -3,12 +3,20 @@ import React from "react";
 const AboutSection = () => {
   return (
     <div className="h-screen flex">
-      <div className="div1 bg-black w-2/4 flex justify-center items-center float-right">
-        <div className="w-5/6 h-5/6 bg-white"></div>
+      <div className="div1 relative bg-red-900 w-2/4 flex justify-center items-center float-right">
+        <div className="w-5/6 h-5/6 bg-red-900 relative z-10"></div>
+        <div className="absolute top-0 bottom-0 left-0 w-1 bg-yellow-500"></div>
+        <div className="absolute top-0 bottom-0 right-0 w-0.5 bg-yellow-500"></div>
+        <div className="absolute top-0 left-0 right-0 h-1 bg-yellow-500"></div>
+        <div className="absolute bottom-0 left-0 right-0 h-1 bg-yellow-500"></div>
       </div>
 
-      <div className="div2 bg-black w-2/4 flex justify-center items-center float-left">
-        <div className="w-5/6 h-5/6 bg-white"></div>
+      <div className="div2 relative bg-red-900 w-2/4 flex justify-center items-center float-left">
+        <div className="w-5/6 h-5/6 bg-red-900 relative z-10"></div>
+        <div className="absolute top-0 bottom-0 left-0 w-0.5 bg-yellow-500"></div>
+        <div className="absolute top-0 bottom-0 right-0 w-1 bg-yellow-500"></div>
+        <div className="absolute top-0 left-0 right-0 h-1 bg-yellow-500"></div>
+        <div className="absolute bottom-0 left-0 right-0 h-1 bg-yellow-500"></div>
       </div>
     </div>
   );
