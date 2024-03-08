0669792159809fcade650f6c1014ea2309809670
RaymondJAT <rjat081700@gmail.com>
2024-03-08 15:05:09 +0800

    semi-final footer

--- first-project/src/components/HomePage/contact/ContactSection.jsx
+++ first-project/src/components/HomePage/contact/ContactSection.jsx
@@ -1,109 +1,148 @@
 import React from "react";
 import {
-  FaFacebook,
-  FaTwitter,
-  FaInstagram,
-  FaGlobe,
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaInstagramSquare,
+  FaGlobeAsia,
   FaMapMarkerAlt,
   FaPhoneAlt,
   FaEnvelope,
   FaCopyright,
 } from "react-icons/fa";
 const ContactPage = () => {
+  const color = {
+    color: "white",
+  };
+
   return (
-    <section>
-      <div className="main-content">
-        <div className="left box">
-          <h2>About Us</h2>
-          <div className="content">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-              voluptates accusamus tempore consectetur recusandae ipsum
-              laboriosam assumenda ea alias laudantium illum, blanditiis
-              voluptatibus impedit officia.
+    <footer className="w-full bottom-0 bg-stone-950" style={color}>
+      {/* left box */}
+      <div className="main-content flex">
+        <div className="left box basis-1/2 py-3 px-5">
+          <h2 className="text-lg font-semibold uppercase">About Us</h2>
+          <div className="content mt-5 relative before:absolute before:-top-3 before:h-1 before:w-full before:bg-stone-950 after:absolute after:-top-3 after:h-1 after:w-1/6 after:bg-red-600">
+            <p className="text-base">
+              5L SOLUTIONS SUPPLY AND ALLIED SERVICES is company who provides
+              technical equipment and services in the field of information
+              Technology in offices, residences, companies and manufacturing
+              organization across various industries.
             </p>
-            <div className="social">
-              <a href="#">
-                <span className="fa facebook">
-                  {" "}
-                  <FaFacebook />
-                </span>
+            <div className="social mt-5 flex">
+              <a href="#" className="py-0 px-2">
+                <FaFacebookSquare
+                  size={35}
+                  className=" bg-stone-800 leading-10 text-center text-lg rounded-md transition duration-300 hover:bg-red-600"
+                />
               </a>
-              <a href="#">
-                <span className="fa twitter">
-                  {" "}
-                  <FaTwitter />
-                </span>
+              <a href="#" className="py-0 px-2">
+                <FaTwitterSquare
+                  size={35}
+                  className=" bg-stone-800 leading-10 text-center text-lg rounded-md transition duration-300 hover:bg-red-600"
+                />
               </a>
-              <a href="#">
-                <span className="fa instagram">
-                  {" "}
-                  <FaInstagram />
-                </span>
+              <a href="#" className="py-0 px-2">
+                <FaInstagramSquare
+                  size={35}
+                  className=" bg-stone-800 leading-10 text-center text-lg rounded-md transition duration-300 hover:bg-red-600"
+                />
               </a>
-              <a href="#">
-                <span className="fa globe">
-                  {" "}
-                  <FaGlobe />
-                </span>
+              <a href="#" className="py-0 px-2">
+                <FaGlobeAsia
+                  size={35}
+                  className=" bg-stone-800 leading-10 text-center text-lg rounded-md transition duration-300 hover:bg-red-600"
+                />
               </a>
             </div>
           </div>
         </div>
-        <div className="center box">
-          <h2>Address</h2>
-          <div className="content">
-            <div className="place">
-              <span className="fa marker">
-                <FaMapMarkerAlt />
+        {/* center box */}
+        <div className="center box basis-1/2 py-3 px-5">
+          <h2 className="text-lg font-semibold uppercase">Company Address</h2>
+          <div className="content mt-5 relative flex flex-col before:absolute before:-top-3 before:h-1 before:w-full before:bg-stone-950 after:absolute after:-top-3 after:h-1 after:w-1/6 after:bg-red-600">
+            <div className="place flex items-center">
+              <FaMapMarkerAlt
+                size={35}
+                className="bg-stone-900 leading-10 text-center rounded-md transition duration-300 cursor-pointer hover:bg-red-600"
+              />
+              <span className="text text-lg pl-3 font-medium">
+                Put your Address, here
               </span>
-              <span className="text">Put your Address, here</span>
             </div>
-            <div className="phone">
-              <span className="fa phone">
-                <FaPhoneAlt />
+
+            <div className="phone my-9 mx-0 flex items-center">
+              <FaPhoneAlt
+                size={35}
+                className="bg-stone-900 leading-10 text-center rounded-md transition duration-300 cursor-pointer hover:bg-red-600"
+              />
+              <span className="text text-lg pl-3 font-medium">
+                123-123-1234
               </span>
-              <span className="text">123-123-1234</span>
             </div>
-            <div className="email">
-              <span className="fa email">
-                <FaEnvelope />
+
+            <div className="email flex items-center">
+              <FaEnvelope
+                size={35}
+                className="bg-stone-900 leading-10 text-center rounded-md transition duration-300 cursor-pointer hover:bg-red-600"
+              />
+              <span className="text text-lg pl-3 font-medium">
+                sampleEmail@email.com
               </span>
-              <span className="text">sampleEmail@email.com</span>
             </div>
           </div>
         </div>
-        <div className="right box">
-          <h2>Contact Us</h2>
-          <div className="content">
+        {/* right box */}
+        <div className="right box basis-1/2 py-3 px-5">
+          <h2 className="text-base font-semibold uppercase">Contact Us</h2>
+          <div className="content mt-5 relative before:absolute before:-top-3 before:h-1 before:w-full before:bg-stone-950 after:absolute after:-top-3 after:h-1 after:w-1/6 after:bg-red-600">
             <form action="#">
               <div className="email">
-                <div className="text">Email</div>
-                <input type="email" name="" id="" required />
+                <div className="text text-base mb-2">Email</div>
+                <input
+                  type="email"
+                  name=""
+                  id=""
+                  className="w-full text-base bg-stone-900 pl-3 border border-solid border-stone-700 focus:outline-red-600 h-9"
+                  required
+                />
               </div>
-              <div className="msg">
-                <div className="text">Message</div>
-                <textarea name="" id="" cols="25" rows="3"></textarea>
+              <div className="msg mt-3">
+                <div className="text text-base mb-2">Message</div>
+                <textarea
+                  name=""
+                  id=""
+                  cols="25"
+                  rows="3"
+                  className="w-full text-base bg-stone-900 pl-3 border border-solid border-stone-700 focus:outline-red-600"
+                ></textarea>
               </div>
-              <div className="btn">
-                <button type="submit">Send</button>
+              <div className="btn mt-3">
+                <button
+                  type="submit"
+                  className="w-full h-10 outline-none border-none bg-red-600 text-base font-medium cursor-pointer transition duration-300 text-black hover:bg-white"
+                >
+                  Send
+                </button>
               </div>
             </form>
           </div>
         </div>
       </div>
       <div className="bottom">
-        <center>
-          <span className="credit">
-            Created By <a href="#">RaymondJAT</a> |{" "}
-          </span>
-          <span className="fa copyright">
+        <center className="bg-stone-900 p-2 text-sm whitespace-nowrap">
+          <span className="credit text-stone-500 inline-block">
+            Created By{" "}
+            <a href="#" className="text-red-700 no-underline hover:underline">
+              RaymondJAT
+            </a>{" "}
+            |{" "}
+          </span>{" "}
+          <span className="fa copyright inline-block">
             <FaCopyright />
           </span>
-          <span>2024 All rights reserved.</span>
+          <span className="inline-block">2024 All rights reserved.</span>
         </center>
       </div>
-    </section>
+    </footer>
   );
 };
 
