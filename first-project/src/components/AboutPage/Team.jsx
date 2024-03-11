501c9c2953c6354cf79bfbdb9cda2c7466482267
RaymondJAT <rjat081700@gmail.com>
2024-03-11 11:54:15 +0800

    latest

--- first-project/src/components/AboutPage/Team.jsx
+++ first-project/src/components/AboutPage/Team.jsx
@@ -50,6 +50,16 @@ const Team = () => {
           <div className="desig my-2 mx-0">position</div>
         </div>
       </div>
+
+      <div className="contact map p-5 bg-white shadow-neutral-950">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d662.143061630113!2d121.06078397321383!3d14.338243843836217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d713f7b6e5ad%3A0xeafcedaf1c936365!2s5L%20Solutions%20Main%20Office!5e0!3m2!1sen!2sph!4v1710119281740!5m2!1sen!2sph"
+          className="a border-none w-full h-96"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </div>
   );
 };
