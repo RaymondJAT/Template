0cd0eb404073860e57a8560a36eab2d94e3feba9
RaymondJAT <rjat081700@gmail.com>
2024-03-15 16:33:42 +0800

    [Navigation][Revised]

--- first-project/src/components/HomePage/navbar/Nav.jsx
+++ first-project/src/components/HomePage/navbar/Nav.jsx
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.cjs";
 import logo from "../../../assets/5L/5L_logo-Red.png";
 
 const Nav = () => {
-  const [nav, setNav] = useState(false);
+  let Links = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/about" },
+    { name: "Service", link: "/services" },
+    { name: "Products", link: "/products" },
+    { name: "News", link: "/news" },
+    { name: "Contact", link: "/contacts" },
+  ];
+
+  let [isNav, isSetNav] = useState(false);
 
   const handleNavToggle = () => {
-    setNav(!nav);
-  };
-
-  const closeNavMenu = () => {
-    setNav(false);
+    isSetNav(!isNav);
   };
 
   useEffect(() => {
+    AOS.init();
+  });
+
+  useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth > 768 && nav) {
-        setNav(false);
+      if (window.innerWidth > 768 && isNav) {
+        isSetNav(false);
       }
     };
 
@@ -26,99 +36,48 @@ const Nav = () => {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [nav]);
+  }, [isNav]);
 
   const header = {
-    background: `rgba(0, 0, 0, 0.9)`,
+    background: `rgba(0, 0, 0, 0.95)`,
   };
 
   return (
     <div
-      className="absolute flex justify-between items-center h-24 w-full mx-auto z-50 px-4 text-white"
-      style={header}
+      className="absolute flex justify-between items-center h-24 w-full mx-auto px-4 bg-black text-white"
+      // style={header}
     >
-      <h1 className="mx-7 cursor-pointer ">
+      <div className="mx-7 cursor-pointer z-20">
         <img
           src={logo}
           alt="company logo"
-          className="h-12 flex flex-col justify-between items-center max-w-[1240px]"
+          className="h-8 sm:h-10 flex flex-col justify-between items-center max-w-[1240px]"
         />
-      </h1>
-      <ul className="hidden md:flex cursor-pointer mx-5 uppercase text-sm">
-        <li className="p-7 hover:text-red-500 ease-in duration-300">
-          <Link to="/" onClick={closeNavMenu}>
-            Home
-          </Link>
-        </li>
-        <li className="p-7 hover:text-red-500 ease-in duration-300">
-          <Link to="/about" onClick={closeNavMenu}>
-            About
-          </Link>
-        </li>
-        <li className="p-7 hover:text-red-500 ease-in duration-300">
-          <Link to="/services" onClick={closeNavMenu}>
-            Services
-          </Link>
-        </li>
-        <li className="p-7 hover:text-red-500 ease-in duration-300">
-          <Link to="/products" onClick={closeNavMenu}>
-            Products
-          </Link>
-        </li>
-        <li className="p-7 hover:text-red-500 ease-in duration-300">
-          <Link to="/news" onClick={closeNavMenu}>
-            News
-          </Link>
-        </li>
-        <li className="p-7 hover:text-red-500 ease-in duration-300">
-          <Link to="/contacts" onClick={closeNavMenu}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+      </div>
+
       <div
         onClick={handleNavToggle}
-        className="block md:hidden h-20 pt-7 mx-5 cursor-pointer"
+        className="block md:hidden h-20 pt-7 mx-5 cursor-pointer z-20"
       >
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {isNav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      {/* linking responsive */}
+
       <ul
-        className={`absolute top-24 left-0 w-full bg-black bg-opacity-90 transition-max-height duration-300 ${
-          nav ? "max-h-screen" : "max-h-0"
-        } uppercase cursor-pointer text-sm py-5 text-center overflow-hidden`}
-        style={{ display: nav || nav === undefined ? "block" : "none" }}
+        className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-10 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in uppercase text-center py-0 px-6 ${
+          isNav ? "top-24" : "top-[-490px]"
+        }`}
+        style={header}
       >
-        <li className="p-4 hover:text-red-500 ease-in duration-300">
-          <Link to="/" onClick={closeNavMenu}>
-            Home
-          </Link>
-        </li>
-        <li className="p-4 hover:text-red-500 ease-in duration-300">
-          <Link to="/about" onClick={closeNavMenu}>
-            About
-          </Link>
-        </li>
-        <li className="p-4 hover:text-red-500 ease-in duration-300">
-          <Link to="/services" onClick={closeNavMenu}>
-            Services
-          </Link>
-        </li>
-        <li className="p-4 hover:text-red-500 ease-in duration-300">
-          <Link to="/products" onClick={closeNavMenu}>
-            Products
-          </Link>
-        </li>
-        <li className="p-4 hover:text-red-500 ease-in duration-300">
-          <Link to="/news" onClick={closeNavMenu}>
-            News
-          </Link>
-        </li>
-        <li className="p-4 hover:text-red-500 ease-in duration-300">
-          <Link to="/contacts" onClick={closeNavMenu}>
-            Contact
-          </Link>
-        </li>
+        {Links.map((link) => (
+          <li className="md:ml-8 md:my-0 my-7 font-semibold text-sm py-1 px-0">
+            <a
+              href={link.link}
+              className="text-white hover:text-red-600 duration-500"
+            >
+              {link.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
