faee6b01543507cda69fde3a4aa030836cd899be
RaymondJAT <rjat081700@gmail.com>
2024-03-18 14:33:26 +0800

    [Navigation][Final]

--- first-project/src/components/HomePage/navbar/Nav.jsx
+++ first-project/src/components/HomePage/navbar/Nav.jsx
@@ -43,44 +43,63 @@ const Nav = () => {
   };
 
   return (
-    <div
-      className="absolute flex justify-between items-center h-24 w-full mx-auto px-4 bg-black text-white"
-      // style={header}
-    >
-      <div className="mx-7 cursor-pointer z-20">
-        <img
-          src={logo}
-          alt="company logo"
-          className="h-8 sm:h-10 flex flex-col justify-between items-center max-w-[1240px]"
-        />
-      </div>
-
+    <div className="shadow-md w-full fixed top-0 left-0">
       <div
-        onClick={handleNavToggle}
-        className="block md:hidden h-20 pt-7 mx-5 cursor-pointer z-20"
+        className="md:flex items-center justify-between bg-black text-white py-7 md:px-10 px-7"
+        // style={header}
       >
-        {isNav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        <div className="mx-7 flex items-center cursor-pointer">
+          <img
+            src={logo}
+            alt="company logo"
+            className="h-8 sm:h-10 flex flex-col justify-between items-center max-w-[1240px]"
+          />
+        </div>
+        <div
+          onClick={handleNavToggle}
+          className="block absolute right-8 top-2 md:hidden h-20 pt-7 mx-5 cursor-pointer"
+        >
+          {isNav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-5 absolute md:static md:z-auto -z-10 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in capitalize text-center py-0 px-6 ${
+            isNav ? "top-20" : "top-[-490px]"
+          }`}
+          style={header}
+        >
+          {Links.map((link) => (
+            <li className="md:ml-8 md:my-0 my-7  text-sm py-1 px-0 z-10">
+              <a
+                href={link.link}
+                className="text-white hover:text-red-600 hover:font-bold duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <ul
-        className={`md:flex md:items-center md:pb-0 pb-5 absolute md:static md:z-auto z-10 left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in capitalize text-center py-0 px-6 ${
-          isNav ? "top-24" : "top-[-490px]"
-        }`}
-        style={header}
-      >
-        {Links.map((link) => (
-          <li className="md:ml-8 md:my-0 my-7  text-sm py-1 px-0 z-50">
-            <a
-              href={link.link}
-              className="text-white hover:text-red-600 hover:font-bold duration-500 z-50"
-            >
-              {link.name}
-            </a>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
 
 export default Nav;
+
+{
+  /*<div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+      <ion-icon name={open ? 'close':'menu'}></ion-icon>
+      </div>
+
+   <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+{
+  Links.map((link)=>(
+    <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+      <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
+    </li>
+  ))
+}
+<Button>
+  Get Started
+</Button>
+</ul> */
+}
